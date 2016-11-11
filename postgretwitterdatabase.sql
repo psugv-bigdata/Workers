d42fde9b6475619ef7a7e96fa89792d4fc14cf69
psugv-bigdata <adrian@psu.edu>
2016-11-11 12:23:32 -0500

    First version of twitter database in Postgres

---
+++ postgretwitterdatabase.sql
@@ -0,0 +1,41 @@
+﻿-- run this as a different user ftom "twitter"
+-- one needs to disconnect from the database "twitter"
+
+DROP DATABASE IF EXISTS twitter;
+DROP USER IF EXISTS twitter;
+
+CREATE USER twitter WITH PASSWORD 'twitter';
+CREATE DATABASE twitter OWNER twitter;
+    
+DROP TABLE IF EXISTS twitterdata;
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS topics;
+
+-- change the password after you finish
+-- re-connect with twitter
+
+CREATE TABLE topics (
+	 id serial
+	,topic text
+	,category text
+	,searchterm text
+	,CONSTRAINT toppk PRIMARY KEY (id)  
+);
+CREATE TABLE tweets (
+	 id serial 
+	,topicid integer
+	,creator text
+	,createdat timestamp
+	,rawtweet json
+	,CONSTRAINT tspk PRIMARY KEY (id) 
+	,CONSTRAINT tsfk FOREIGN KEY (topicid) REFERENCES public.topics(id) 
+);
+CREATE TABLE twitterdata (  
+	 id serial 
+	,lexcategory text
+	,relevance double precision
+	,sentiment text
+	,tweetid integer
+	,CONSTRAINT twpk PRIMARY KEY (id)
+	,CONSTRAINT twfk FOREIGN KEY (tweetid ) REFERENCES public.tweets (id) 
+);
